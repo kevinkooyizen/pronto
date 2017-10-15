f7e628948b9efca90c88fe1f9b5291566007b5f3
Koo Yi Zen <kooyizen@hotmail.com>
2017-10-15 22:39:26 +0800

    cloudinary configuration

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,15 +12,27 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [100, 150, :north]
   end
 
+  version :display do
+    process :eager => true
+    process :resize_to_fill => [200, 200, :north]
+  end
+  # call image like this:
+  # <%= image_tag @user.avatar_url(:display) %>
+
+  version :thumbnail do
+    process :eager => true
+    process :resize_to_fit => [50, 50]
+  end
+
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
@@ -38,9 +50,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [950, 850]
-  end
+  # version :thumb do
+  #   process resize_to_fit: [950, 850]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
