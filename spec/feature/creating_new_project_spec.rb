35ab56af9eb3dcb4e2b546fbde61aabc891a144e
Koo Yi Zen <kooyizen@hotmail.com>
2017-10-19 17:28:18 +0800

    front end heroku

--- spec/feature/creating_new_project_spec.rb
+++ spec/feature/creating_new_project_spec.rb
@@ -32,4 +32,14 @@ feature "Signing in" do
     click_button "Create Project"
     expect(page).to have_content "Name can't be blank"
   end
+
+  scenario "Signing in and creating a project", js: true do
+    visit '/sign_in'
+    within(".sign-in") do
+      fill_in 'Email', with: 'bob@example.com'
+      fill_in 'Password', with: '123457'
+    end
+    click_button 'Sign In'
+    expect(page).to have_content 'Invalid'
+  end
 end
